68d62e1d52a66fc1bdfd36931cd9b44d589940d6
YUHAN KO <rhdbgks789@gmail.com>
2025-10-20 11:26:32 +0900

    고유한/251020/Project Init

---
+++ client/src/views/AboutView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <h2>{{ title }}</h2>
+    </div>
+</template>
+
+<script setup>
+import axios from "axios";
+import { ref, onBeforeMount } from "vue";
+
+const title = ref("");
+
+const getTitle = async () => {
+    let result = await axios.get("/api/board").catch(console.log);
+    title.value = result.data.title;
+};
+
+onBeforeMount(() => {
+    getTitle();
+});
+</script>
+
+<style>
+@media (min-width: 1024px) {
+    .about {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
